31fcc8b0905f069c02ae28cf6b4844c433f3ed6e
Victor Planas <victor.planas@mercadolivre.com>
2021-11-01 14:47:40 -0300

    Add card content

---
+++ src/bd/content.js
@@ -0,0 +1,22 @@
+const content = [
+    {
+        id: 1,
+        title: 'Grow Together',
+        body: 'Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.',
+        image: 'https://www.robotlab.com/hubfs/Nao%20Power%20V6-1-%20darker.png',
+    },
+    {
+        id: 2,
+        title: 'Flowing Conversations',
+        body: "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
+        image: 'https://www.cyberstein.com/wp-content/themes/Cyberstein2021/theme/images/K-na.png',
+    },
+    {
+        id: 3,
+        title: 'Your Users',
+        body: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
+        image: 'https://www.soitron.com/wp-content/uploads/2019/07/robot-01.png',
+    },
+]
+
+export default content
